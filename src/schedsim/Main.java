07e1818a84d63a2fa2624ce97e8917e94b78d04f
jindvirB <jbeni951@mtroyal.ca>
2025-12-02 23:03:49 -0700

    fixed main file

--- src/schedsim/Main.java
+++ src/schedsim/Main.java
@@ -3,11 +3,12 @@ package schedsim;
 import java.util.LinkedList;
 
 public class Main {
-	int time = 0;
-	int current_process = -1;
-	Process[] process_table = new Process[128];
-	LinkedList<Integer> process_queue;
-	LinkedList<Event> event_queue;
-	
-	
+    public static void main(String[] args) {
+        int time = 0;
+        int current_process = -1;
+        Process[] process_table = new Process[128];
+        LinkedList<Integer> process_queue;
+        LinkedList<Event> event_queue;
+
+    }
 }
