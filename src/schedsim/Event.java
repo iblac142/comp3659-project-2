bb4016092c111e2cc4cf21613a3683c5f77a193f
jindvirB <jbeni951@mtroyal.ca>
2025-12-03 22:13:04 -0700

    added time to event class atributes

--- src/schedsim/Event.java
+++ src/schedsim/Event.java
@@ -1,15 +1,22 @@
 package schedsim;
 
 public class Event {
-	String type;
-	int process;
-	
-	Event(String type) {
-		this.type = type;
-	}
-	
-	Event(String type, int process) {
-		this.type = type;
-		this.process = process;
-	}
+    String type;
+    int process;
+    int time;
+
+    Event(String type) {
+        this.type = type;
+    }
+
+    Event(String type, int process) {
+        this.type = type;
+        this.process = process;
+    }
+
+    public Event(String type, int process, int time) {
+        this.type = type;
+        this.process = process;
+        this.time = time;
+    }
 }
