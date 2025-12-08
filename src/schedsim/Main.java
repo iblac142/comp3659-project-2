ce93dc7deb88340a6b76baccf1ecff52e9171716
jindvirB <jbeni951@mtroyal.ca>
2025-12-08 01:06:27 -0700

    removed unused RR if statment in main

--- src/schedsim/Main.java
+++ src/schedsim/Main.java
@@ -12,58 +12,52 @@ public class Main {
         Inputs inputs = new Inputs();
         Scanner in = new Scanner(System.in);
         int selected_algorithm = inputs.intInput(in,
-                "Select an algorithm.\n"
-                + "0 - First Come First Serve\n"
-                + "1 - Simple Priority\n"
-                + "2 - Shortest Job First\n"
-                + "3 - Pre-Emptive Shortest Job First\n"
-                + "4 - Round Robin (not implemented)",
+                "Select an algorithm.\n" + "0 - First Come First Serve\n" + "1 - Simple Priority\n"
+                        + "2 - Shortest Job First\n" + "3 - Pre-Emptive Shortest Job First\n"
+                        + "4 - Round Robin (not implemented)",
                 0, 3);
 
         if (inputs.getProcesses(in, process_table) >= 0) {
-        	
-        	for (int j = 0; process_table[j] != null; j += 1) {
-            	Event e = new Event("process_arrives", j, process_table[j].getArrival_time());
-            	event_queue.add(e);
-            	event_queue.sort(Comparator.comparingInt(Event::getTime));
-            }
-        	
-	        if (selected_algorithm == 0) {
-	            FIFO fifo = new FIFO(process_table, event_queue);
-	
-	            fifo.runSimulator();
-	            fifo.outputStatistics();
-	        }
-	
-	        if (selected_algorithm == 1) {
-	            SimplePriority prio = new SimplePriority(process_table, event_queue);
-	
-	            prio.runSimulator();
-	            prio.outputStatistics();
-	        }
-	
-	        if (selected_algorithm == 2) {
 
-	            SJF sjf = new SJF(process_table, event_queue,
-	            		(float) inputs.intInput(in, "Input an \"a\" % value: (0-100)", 0, -1) / 100,
-	            		inputs.intInput(in, "Input the initial prediction value: (0+)", 0, -1));
-	
-	            sjf.runSimulator();
-	            sjf.outputStatistics();
-	        }
-	
-	        if (selected_algorithm == 3) {
-	            SRJF srjf = new SRJF(process_table, event_queue,
-	            		(float) inputs.intInput(in, "Input an \"a\" % value: (0-100)", 0, -1) / 100,
-	            		inputs.intInput(in, "Input the initial prediction value: (0+)", 0, -1));
-	
-	            srjf.runSimulator();
-	            srjf.outputStatistics();
-	        }
-	
-	        if (selected_algorithm == 4) {
-	            //q = inputs.intInput(in, "Input a \"q\" value: (1+)", 1, -1);
-	        }
+            for (int j = 0; process_table[j] != null; j += 1) {
+                Event e = new Event("process_arrives", j, process_table[j].getArrival_time());
+                event_queue.add(e);
+                event_queue.sort(Comparator.comparingInt(Event::getTime));
+            }
+
+            if (selected_algorithm == 0) {
+                FIFO fifo = new FIFO(process_table, event_queue);
+
+                fifo.runSimulator();
+                fifo.outputStatistics();
+            }
+
+            if (selected_algorithm == 1) {
+                SimplePriority prio = new SimplePriority(process_table, event_queue);
+
+                prio.runSimulator();
+                prio.outputStatistics();
+            }
+
+            if (selected_algorithm == 2) {
+
+                SJF sjf = new SJF(process_table, event_queue,
+                        (float) inputs.intInput(in, "Input an \"a\" % value: (0-100)", 0, -1) / 100,
+                        inputs.intInput(in, "Input the initial prediction value: (0+)", 0, -1));
+
+                sjf.runSimulator();
+                sjf.outputStatistics();
+            }
+
+            if (selected_algorithm == 3) {
+                SRJF srjf = new SRJF(process_table, event_queue,
+                        (float) inputs.intInput(in, "Input an \"a\" % value: (0-100)", 0, -1) / 100,
+                        inputs.intInput(in, "Input the initial prediction value: (0+)", 0, -1));
+
+                srjf.runSimulator();
+                srjf.outputStatistics();
+            }
+
         }
 
     }
