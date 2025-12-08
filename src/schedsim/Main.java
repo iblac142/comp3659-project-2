abdea6303da02dd261aee4b714a54e32133f0abf
iain <iblac142@mtroyal.ca>
2025-12-07 23:00:37 -0700

    Update Main.java

--- src/schedsim/Main.java
+++ src/schedsim/Main.java
@@ -5,62 +5,57 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int time = 0;
-        int current_process = -1;
-
-        int a; // for sjf
-        int f; // for sjf
-        int q; // for rr
-
         Process[] process_table = new Process[128];
-        LinkedList<Integer> process_queue;
         LinkedList<Event> event_queue = new LinkedList<Event>();
 
         Inputs inputs = new Inputs();
         Scanner in = new Scanner(System.in);
         int selected_algorithm = inputs.intInput(in,
-                "Select an algorithm.\n0 - First Come First Serve\n1 - Simple Priority\n2 - Shortest Job First\n3 - Pre-Emptive Shortest Job First\n4 - Round Robin",
+                "Select an algorithm.\n"
+                + "0 - First Come First Serve\n"
+                + "1 - Simple Priority\n"
+                + "2 - Shortest Job First\n"
+                + "3 - Pre-Emptive Shortest Job First\n"
+                + "4 - Round Robin",
                 0, 4);
 
-        inputs.getProcesses(in, process_table);
+        if (inputs.getProcesses(in, process_table) >= 0) {
+	        if (selected_algorithm == 0) {
+	            FIFO fifo = new FIFO(process_table, event_queue);
+	
+	            fifo.runSimulator();
+	            fifo.outputStatistics();
+	        }
+	
+	        if (selected_algorithm == 1) {
+	            SimplePriority prio = new SimplePriority(process_table, event_queue);
+	
+	            prio.runSimulator();
+	            prio.outputStatistics();
+	        }
+	
+	        if (selected_algorithm == 2) {
 
-        if (selected_algorithm == 0) {
-            // TODO
-            FIFO fifo = new FIFO(process_table, event_queue);
-
-            fifo.runSimulator();
-            fifo.outputStatistics();
-        }
-
-        if (selected_algorithm == 1) {
-            SimplePriority prio = new SimplePriority(process_table, event_queue);
-
-            prio.runSimulator();
-            prio.outputStatistics();
-        }
-
-        if (selected_algorithm == 2) {
-            a = inputs.intInput(in, "Input an \"a\" % value: (0-100)", 0, -1);
-            f = inputs.intInput(in, "Input the initial prediction value: (0+)", 0, -1);
-
-            SJF sjf = new SJF(process_table, event_queue, a, f);
-
-            sjf.runSimulator();
-            sjf.outputStatistics();
-        }
-
-        if (selected_algorithm == 3) {
-            a = inputs.intInput(in, "Input an \"a\" % value: (0-100)", 0, -1);
-            f = inputs.intInput(in, "Input the initial prediction value: (0+)", 0, -1);
-
-            SRJF srjf = new SRJF(process_table, event_queue, a, f);
-
-            srjf.runSimulator();
-            srjf.outputStatistics();
-        }
-
-        if (selected_algorithm == 4) {
-            q = inputs.intInput(in, "Input a \"q\" value: (1+)", 1, -1);
+	            SJF sjf = new SJF(process_table, event_queue, inputs.intInput(in, 
+	            		"Input an \"a\" % value: (0-100)", 0, -1),
+	            		inputs.intInput(in, "Input the initial prediction value: (0+)", 0, -1));
+	
+	            sjf.runSimulator();
+	            sjf.outputStatistics();
+	        }
+	
+	        if (selected_algorithm == 3) {
+	            SRJF srjf = new SRJF(process_table, event_queue,
+	            		inputs.intInput(in, "Input an \"a\" % value: (0-100)", 0, -1),
+	            		inputs.intInput(in, "Input the initial prediction value: (0+)", 0, -1));
+	
+	            srjf.runSimulator();
+	            srjf.outputStatistics();
+	        }
+	
+	        if (selected_algorithm == 4) {
+	            //q = inputs.intInput(in, "Input a \"q\" value: (1+)", 1, -1);
+	        }
         }
 
     }
